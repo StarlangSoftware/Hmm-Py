5c24890c355038e8f9d140c1d5e40b7f769c9052
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-08-10 09:04:14 +0300

    Updated Hmm repository translation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-Hmm',
-    version='1.0.0',
+    version='1.0.1',
     packages=['Hmm'],
     url='https://github.com/olcaytaner/Hmm-Py',
     license='',
