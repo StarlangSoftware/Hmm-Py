2c21145be040ce883dba9984e243892d22f65158
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:32:19 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-Hmm',
     version='1.0.5',
     packages=['Hmm'],
-    url='https://github.com/olcaytaner/Hmm-Py',
+    url='https://github.com/StarlangSoftware/Hmm-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Hidden Markov Model Library',
     install_requires=['NlpToolkit-Math', 'NlpToolkit-DataStructure']
 )
