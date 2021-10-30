4aba24d860876e145bb97599458aaa9af7ccebc0
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:40:07 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Hmm',
-    version='1.0.5',
+    version='1.0.6',
     packages=['Hmm'],
     url='https://github.com/StarlangSoftware/Hmm-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Hidden Markov Model Library',
-    install_requires=['NlpToolkit-Math', 'NlpToolkit-DataStructure']
+    install_requires=['NlpToolkit-Math', 'NlpToolkit-DataStructure'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
